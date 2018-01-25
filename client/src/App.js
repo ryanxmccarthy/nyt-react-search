3b86a7011e4dcbe518ac271f0d1fb40a51559a5d
Ryan McCarthy <ryanxmccarthy@gmail.com>
2018-01-25 11:46:16 -0500

    router changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
+import Articles from "./pages/Articles";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
@@ -10,9 +9,7 @@ const App = () =>
     <div>
       <Nav />
       <Switch>
-        <Route exact path="/" component={Books} />
-        <Route exact path="/books" component={Books} />
-        <Route exact path="/books/:id" component={Detail} />
+        <Route exact path="/" component={Articles} />
         <Route component={NoMatch} />
       </Switch>
     </div>
